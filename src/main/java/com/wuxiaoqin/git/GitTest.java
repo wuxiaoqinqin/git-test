19932c996115b4dddef58be08476f4fa0e386123
wuxiaoqin <2052256855@qq.com>
2022-02-11 20:55:18 +0800

    IDEA project first commit

---
+++ src/main/java/com/wuxiaoqin/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wuxiaoqin.git;
+
+/**
+ * @Title:
+ * @Author 吴晓钦
+ * @Date 2022/2/11 20:51
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
